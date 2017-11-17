b23505fbef77b61f65b4cd0f574cb545364b134e
Unknown <58070033@kmitl.ac.th>
2017-11-17 23:23:31 +0700

    fix layout

--- app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
+++ app/src/main/java/cc/somkiat/basicunittesting/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.eiei);
         getSupportActionBar().hide();
 
         emailValidation = new EmailValidation();
